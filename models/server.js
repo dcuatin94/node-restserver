e4d7d1e852d17a2812fabc25c6fc34181cdd8937
Daniel Cuatin <dannyel.cuatin@gmail.com>
2023-03-24 18:44:35 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload');
 class Server {
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 8080;
         
         this.paths = {
             auth: '/api/auth',
